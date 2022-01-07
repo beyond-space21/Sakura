2af898377ab5a7a2d90ed13a792aa070f601b2a3
s4njeev <s4njeev.av@gmail.com>
2022-01-07 22:38:09 +0530

    sakura_web_ui

---
+++ android/app/src/main/kotlin/com/example/sakuro_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sakuro_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class SakuraHome extends StatelessWidget {
+  const SakuraHome({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
